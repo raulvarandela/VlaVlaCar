cf625da62c5e3cb2c3cb3c9d8965a8043ceda02d
Raul Varandela <raulvarandela@protonmail.com>
2020-10-10 12:15:40 +0200

    Añadido la lógica de negocio

--- app/src/main/java/com/example/vlavlacar/core/calculadoraCoche.java
+++ app/src/main/java/com/example/vlavlacar/core/calculadoraCoche.java
@@ -2,21 +2,22 @@ package com.example.vlavlacar.core;
 
 public class calculadoraCoche {
     private double consumo;
-    private double kilometros;
-
-    private enum tipoCarretera {
-        A,
-        N,
-        C;}
-    ;
-    private int horas;
+    private int kilometros;
     private int minutos;
 
-    public calculadoraCoche(double consumo, double kilometros, int horas, int minutos, String tipoCarretera) {
-        this.consumo = consumo;
-        this.kilometros = kilometros;
-        this.horas = horas;
-        this.minutos = minutos;
+    private enum TipoCarretera {A, N, C}
+
+    ;
+    private final char[] StrTipoCarretera = {'A', 'N', 'C'};
+    private final int[] velocidades = {100, 80, 50};
+    private TipoCarretera tipoCarretera;
+
+
+    public calculadoraCoche() {
+        this.consumo = 8.0;
+        this.kilometros = 0;
+        this.minutos = 0;
+        this.tipoCarretera = TipoCarretera.A;
     }
 
     public double getConsumo() {
@@ -31,17 +32,10 @@ public class calculadoraCoche {
         return kilometros;
     }
 
-    public void setKilometros(double kilometros) {
+    public void setKilometros(int kilometros) {
         this.kilometros = kilometros;
     }
 
-    public int getHoras() {
-        return horas;
-    }
-
-    public void setHoras(int horas) {
-        this.horas = horas;
-    }
 
     public int getMinutos() {
         return minutos;
@@ -50,4 +44,39 @@ public class calculadoraCoche {
     public void setMinutos(int minutos) {
         this.minutos = minutos;
     }
+
+    public void setTiempo(int horas, int minutos) {
+        this.setMinutos((horas * 60) + minutos);
+    }
+
+    public TipoCarretera getTipoCarretera() {
+        return this.tipoCarretera;
+    }
+
+    public void setTipoCarretera(TipoCarretera tipoCarretera) {
+        this.tipoCarretera = tipoCarretera;
+    }
+
+    public void setTipoCarretera(char tipo) {
+        tipo = Character.toUpperCase(tipo);
+        int temp = 0;
+
+        for (int i = 0; i < StrTipoCarretera.length; i++) {
+            if (StrTipoCarretera[i] == tipo) {
+                temp = i;
+                break;
+            }
+        }
+        this.tipoCarretera = TipoCarretera.values()[temp];
+    }
+
+    public double calcularConsumo() {
+        return (getKilometros() / 100) * getConsumo();
+    }
+
+    public int calcularTiempo() {
+        double velocidad = this.velocidades[this.tipoCarretera.ordinal()];
+        return (int) Math.round((this.getKilometros() / velocidad) * 60.0);
+    }
+
 }
--- app/src/main/java/com/example/vlavlacar/ui/MainActivity.java
+++ app/src/main/java/com/example/vlavlacar/ui/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.vlavlacar.R;
+import com.example.vlavlacar.core.calculadoraCoche;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
         final ImageButton TIEMPO = this.findViewById(R.id.botonTiempo);
@@ -34,12 +37,40 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    calculadoraCoche miCalculadora = new calculadoraCoche();
 
     private void calculaTiempo() {
+        //para pasar los datos
+        final EditText km = this.findViewById(R.id.km);
+        final EditText tipoCarretera = this.findViewById(R.id.carretera);
+        String strKm = km.getText().toString().trim();
+        int kilometros = Integer.parseInt( strKm );
+        char carretera = tipoCarretera.toString().charAt(0);
+        miCalculadora.setKilometros(kilometros);
+        miCalculadora.setTipoCarretera(carretera);
 
+        final EditText min = this.findViewById(R.id.minutos);
+        final EditText h = this.findViewById(R.id.horas);
+        int tiempo = miCalculadora.calcularTiempo();
+        h.setText(Integer.toString( tiempo / 60  ));
+        min.setText(Integer.toString(tiempo % 60));
+
+        calculaConsumo();
     }
 
     private void calculaConsumo(){
+        //para pasar lo datos
+        final EditText km = this.findViewById(R.id.km);
+        String strKm = km.getText().toString().trim();
+        int kilometros = Integer.parseInt( strKm );
+        final EditText ed_consumo = this.findViewById(R.id.consumo);
+        String strConsumo = ed_consumo.getText().toString().trim();
+        double consumo = Double.parseDouble( strConsumo );
+        miCalculadora.setConsumo(consumo);
+        miCalculadora.setKilometros(kilometros);
 
+        ed_consumo.setText(Double.toString(miCalculadora.calcularConsumo()));
     }
+
+
 }
